6629975c2f34af5cca18dc21f232b22feb5a7b21
Nikolaj <nikolaj.tetradov@gmail.com>
2023-05-06 20:58:07 +0300

    add form input

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
+'use client';
 import Image from 'next/image';
 
 export default function Home() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <main className="container mx-auto max-w-4xl">
       <div className="grid grid-cols-2">
@@ -19,6 +24,24 @@ export default function Home() {
           />
         </div>
       </div>
+      <form className="mt-6" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-4">
+          <label className="font-bold">Say something...</label>
+          <input
+            className="px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border border-gray-500 rounded-lg"
+            required
+            type="text"
+            placeholder="Ask me anything..."
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="px-4 py-2 mt-2 text-gray-700 bg-gray-100 border border-gray-700 rounded-lg hover:scale-110 transition-all duration-700"
+        >
+          Send 🚀🙋‍♀️🙋‍♂️⁉️
+        </button>
+      </form>
     </main>
   );
 }
